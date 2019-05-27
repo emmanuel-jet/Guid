0cac28932c0fa4ae0460d6afcd8fc8205f506743
Emmanuel Joseph <32677156+jet774@users.noreply.github.com>
2019-05-27 23:36:27 +0100

    Initial Release

---
+++ config/laratrust_seeder.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+  'role_structure' => [
+    'admin' => [
+      // 'seniorstaff' => 'a',
+      // 'juniorstaff' => 'a',
+      // 'postgraduate' => 'a',
+      // 'undergraduate' => 'a'
+    ],
+    'seniorstaff' => [
+    ],
+    'juniorstaff' => [
+    ],
+    'postgraduate' => [
+    ],
+    'undergraduate' => [
+    ],
+  ],
+  'permissions_map' => [
+    // 'a' => 'all',
+    // 'c' => 'create',
+    // 'r' => 'read',
+    // 'u' => 'update',
+    // 'd' => 'delete'
+  ]
+];
